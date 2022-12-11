bcebc6123a9cbbfe9bf94547229244e1b531843d
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2022-12-11 11:57:01 -0300

    reinitialized files

---
+++ DOM/Suma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOM
+{
+    public class Suma
+    {
+
+        private int a { get; set; }
+        private int b { get; set; }
+
+        public Suma()
+        {
+            this.a = 0;
+            this.b = 0;
+
+
+        }
+
+        public int Sumar(int a, int b)
+        {
+           return a + b;
+
+        }
+
+
+
+
+  
+
+    }
+}
---
+++ Ejemplo/Form1.cs
@@ -0,0 +1,38 @@
+﻿using DOM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Ejemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Suma sum = new Suma();
+
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int r = sum.Sumar(a, b);
+           
+            label1.Text=Convert.ToString(r);
+
+            
+        }
+    }
+}
